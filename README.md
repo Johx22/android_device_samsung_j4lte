70bbed5945dbfe9341d56d6bbb47fb20ec2c1466
Johx22 <johannjose2004@gmail.com>
2022-04-11 16:25:52 +0300

    Upload files

---
+++ README.md
@@ -0,0 +1,12 @@
+# Orange Fox device tree for Samsung Galaxy J4(SM-J400;j4lte)
+
+## How to Build:
+
+```bash
+# Sync the fox_9.0 source
+
+$ . build/envsetup.sh
+$ lunch omni_j4ltejx-eng
+$ mka recoveryimage
+
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+# Allow missing dependencies
+# due to minimal manifest usage
+export TARGET_DEVICE_ALT="j4lte, j4ltejx, j4ltexx"
+export OF_TARGET_DEVICES="j4lte, j4ltejx, j4ltexx"
+
+export FOX_R11="1"
+export FOX_VERSION="R11.1"
+export ALLOW_MISSING_DEPENDENCIES=true
+export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+export LC_ALL="C"
+export OF_FLASHLIGHT_ENABLE=0
+export OF_USE_MAGISKBOOT=1
+export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
+export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION=1
+export FOX_BUILD_TYPE="Beta"
+export OF_MAINTAINER="Johann"
+export OF_MAINTAINER_AVATAR="$PWD/device/samsung/j4ltejx/maintainer.png"
+
+# Add j4lte into lunch entry
+add_lunch_combo omni_j4ltejx-eng

c8cea2aa8631da4ec3836653a7676d4ad6c04397
Johx22 <johannjose2004@gmail.com>
2022-04-30 14:02:27 +0300

    vendorsetup.sh: Add FOX_RECOVERY_INSTALL_PARTITION

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,6 +8,7 @@ export FOX_VERSION="R11.1"
 export ALLOW_MISSING_DEPENDENCIES=true
 export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
 export LC_ALL="C"
+export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/13540000.dwmmc0/by-name/RECOVERY"
 export OF_FLASHLIGHT_ENABLE=1
 export OF_FL_PATH1="/system/flashlight"
 export OF_FL_PATH2=""
